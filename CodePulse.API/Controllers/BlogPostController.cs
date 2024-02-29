52a4fbdcb4ac5f54fdecf6223dc2d750e4a1f95a
A.AJDIR <abdelaliajdir@gmail.com>
2024-02-29 18:04:15 +0100

    Add project files.

---
+++ CodePulse.API/Controllers/BlogPostController.cs
@@ -0,0 +1,74 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPostController(IBlogPostRepository blogPostRepository) {
+            this.blogPostRepository = blogPostRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody]CreateBlogPostRequestDto request)
+        {
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishedDate = request.PublishedDate,
+                ShortDescription = request.ShortDescription,
+                Title = request.Title,
+                UrlHandle = request.UrlHandle,
+            };
+            blogPost =  await blogPostRepository.CreateAsync(blogPost);
+
+            var responce = new BlogPost
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle,
+            };
+
+            return Ok(responce);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBlogPosts()
+        {
+            var blogPosts = await blogPostRepository.GetAllAsync();
+
+            var response = new List<BlogPostDto>();
+            foreach (var blogPost in blogPosts)
+            {
+                response.Add(new BlogPostDto
+                {
+                    Id = blogPost.Id,
+                    Author = blogPost.Author,
+                    Content = blogPost.Content,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    IsVisible = blogPost.IsVisible,
+                    PublishedDate = blogPost.PublishedDate,
+                    ShortDescription = blogPost.ShortDescription,
+                    Title = blogPost.Title,
+                    UrlHandle = blogPost.UrlHandle,
+                });
+            }
+            return Ok(response);
+        }
+    }
+}
---
+++ CodePulse.API/Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CategoryAsync(Category category);
+
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+
+        Task<Category?> GetById(Guid id);
+
+        Task<Category?> UpdateAsync(Category category);
+
+        Task<Category?> DeleteAsync(Guid id);
+    }
+}
